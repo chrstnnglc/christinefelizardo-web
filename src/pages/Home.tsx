1bd8d679d7be0eb11b8edf502a6be21abbd8c82e
chrstnnglc <catfelizardo@gmail.com>
2025-11-09 19:23:23 +0900

    feat: added custom primary color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="h-full w-full flex flex-col items-center justify-center py-8 border border-black border-2">
+        <div className="h-full w-full flex flex-col items-center justify-center py-8 border border-primary border-3">
             <div className="w-full flex flex-col items-center justify-center mt-5">Hello.</div>
         </div>
     );
