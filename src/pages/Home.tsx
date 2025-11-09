abbbacf627bc9c2324b7d8223ca97402ed8893ec
chrstnnglc <catfelizardo@gmail.com>
2025-11-09 14:28:23 +0900

    feat: add home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="h-full w-full flex flex-col items-center justify-center py-8">
+            <div className="w-full mt-5">Hello.</div>
+        </div>
+    );
+};
+
+export default Home;
