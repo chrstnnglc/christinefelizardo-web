2b46552cf113e6c3c1741dba9a76fb89772f4fa4
chrstnnglc <catfelizardo@gmail.com>
2025-11-09 14:38:47 +0900

    feat: added tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
-        <div className="h-full w-full flex flex-col items-center justify-center py-8">
-            <div className="w-full mt-5">Hello.</div>
+        <div className="h-full w-full flex flex-col items-center justify-center py-8 border border-black border-2">
+            <div className="w-full flex flex-col items-center justify-center mt-5">Hello.</div>
         </div>
     );
 };
